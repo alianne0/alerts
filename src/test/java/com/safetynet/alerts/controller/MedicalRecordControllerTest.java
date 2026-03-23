158fbc493ee526c3a58a89493e2ecb8e9c108c0c
Anne Rutecki <anne.rutecki@sherwin.com>
2026-03-23 16:44:47 -0400

    remove todo

--- src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerTest.java
@@ -15,8 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//TODO: clone my repository and test to see if it works
-//TODO: add readme
 import java.util.List;
 import java.util.Optional;
 
